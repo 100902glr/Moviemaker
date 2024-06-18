ffd16c8fedccd424e0240e076a58a6eeb2a7caa7
100902glr <162437257+100902glr@users.noreply.github.com>
2024-06-18 16:59:54 +0200

    updates

--- JavaScripts/home.js
+++ JavaScripts/home.js
@@ -2,45 +2,43 @@ document.addEventListener("DOMContentLoaded", function () {
   var audio = document.getElementById("background-audio");
 
   function playAudio() {
-    audio.play();
-    // Remove event listeners after playing audio to prevent multiple calls
-    document.removeEventListener("click", playAudio);
-    document.removeEventListener("scroll", playAudio);
+      audio.play();
+      document.removeEventListener("click", playAudio);
+      document.removeEventListener("scroll", playAudio);
   }
 
-  // Add event listeners for user interaction
   document.addEventListener("click", playAudio);
   document.addEventListener("scroll", playAudio);
+
+  // Initialize clock
+  setInterval(clock, 1000);
+  clock();
+  // Fetch Gotham news
+  fetchGothamNews();
 });
 
-function clock(){
-                
-    var monthNames = [ "January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December" ]; 
-    var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+function clock() {
+  var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-    var today = new Date();
+  var today = new Date();
 
-    document.getElementById('Date').innerHTML = (dayNames[today.getDay()] + " " + 
-    today.getDate() + ' ' + monthNames[today.getMonth()] + ' ' +today.getFullYear());
+  document.getElementById('Date').innerHTML = (dayNames[today.getDay()] + " " +
+      today.getDate() + ' ' + monthNames[today.getMonth()] + ' ' + today.getFullYear());
 
-    
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    var day = h<11 ? 'AM': 'PM';
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  var day = h < 11 ? 'AM' : 'PM';
 
-    h = h<10? '0'+h: h;
-    m = m<10? '0'+m: m;
-    s = s<10? '0'+s: s;               
+  h = h < 10 ? '0' + h : h;
+  m = m < 10 ? '0' + m : m;
+  s = s < 10 ? '0' + s : s;
 
-    document.getElementById('hours').innerHTML = h;
-    document.getElementById('min').innerHTML = m;
-    document.getElementById('sec').innerHTML = s;
-
-}var inter = setInterval(clock,400);
-
-
+  document.getElementById('hours').innerHTML = h;
+  document.getElementById('min').innerHTML = m;
+  document.getElementById('sec').innerHTML = s;
+}
 
 async function fetchGothamNews() {
   const gothamNews = [
@@ -56,6 +54,3 @@ async function fetchGothamNews() {
       newsFeed.appendChild(li);
   });
 }
-
-fetchGothamNews();
-
